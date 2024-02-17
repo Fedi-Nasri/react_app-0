ca2e2bd67431d0ff059f5c395c73461c4f55ad89
Fedi-Nasri <fedinasri.fsb@gmail.com>
2024-02-17 20:48:31 +0100

    versio 0.0.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 const name ="Fedi"
+
+
+
+
 function App() {
-  return <h1>hello world {name}</h1>
+  return (<>
+  <h1>the king of all king {name}</h1>
+  <h2>look at my creation XD</h2></>);
 }
 
 export default App;
